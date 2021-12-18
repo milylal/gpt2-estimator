8fc0fa5c68317ef43d295c304cafabf7a91dbb56
Mily Lal <milylike@gmail.com>
2021-12-18 23:04:40 +0530

    Update __init__.py

--- gpt2_estimator/__init__.py
+++ gpt2_estimator/__init__.py
@@ -1,6 +1,5 @@
 from .ckpt_restore_hook import RestoreCheckpointHook, get_assignment_map_from_checkpoint
-from .gpt2.src.model import default_hparams
-from .gpt2.src.load_dataset import load_dataset, Sampler
-from .gpt2.src.accumulate import AccumulatingOptimizer
-from . import gpt2
+from .model import default_hparams
+from .load_dataset import load_dataset, Sampler
+from .accumulate import AccumulatingOptimizer
 from gpt2_estimator import gpt2
